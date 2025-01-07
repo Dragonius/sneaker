9e4cff6ea55a8ae69d5a9c17b7a3ede1d9d8c94e
Dragonius <Blackerhawer@hotmail.com>
2025-01-08 00:13:43 +0200

    i would like also amd64 builds on linux

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ yarn build
 cd cmd/sneaker-server
 go generate
 
-platforms=("windows/386" "windows/amd64" "linux/386")
+platforms=("windows/386" "windows/amd64" "linux/386" "linux/amd64")
 
 for platform in "${platforms[@]}"
 do
