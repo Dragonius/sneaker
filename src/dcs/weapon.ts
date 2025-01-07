b6e41ad9c193dde791410e4e9b06782096f73e66
Dragonius <Blackerhawer@hotmail.com>
2025-01-08 01:58:38 +0200

    added more missiles

--- src/dcs/weapon.ts
+++ src/dcs/weapon.ts
@@ -19,6 +19,27 @@ class Missile extends Weapon {
 export const missiles: Record<string, Missile> = {
     "AIM-120C": new Missile("AIM-120C"),
     "AIM-120B": new Missile("AIM-120B"),
+    "AIM-54A-Mk47": new Missile("AIM-54A-Mk47"),
+    "AIM-54A-Mk60": new Missile("AIM-54A-Mk60"),
+    "AIM-54C-Mk47": new Missile("AIM-54C-Mk47"),
+    "AIM-54C-Mk60": new Missile("AIM-54C-Mk60"),
+    "AIM-7F": new Missile("AIM-7F"),
+    "AIM-7MH": new Missile("AIM-7MH"),
+    "AIM-9L": new Missile("AIM-9L"),
+    "BGM-109C Tomahawk": new Missile("BGM-109C Tomahawk"),
+    "MIM-104 Patriot": new Missile("MIM-104 Patriot"),
+    "MIM-23K Hawk": new Missile("MIM-23K Hawk"),
+    "R-27ER (AA-10 Alamo C)": new Missile("R-27ER (AA-10 Alamo C)"),
     "R-33 (AA-9 Amos)": new Missile("R-33 (AA-9 Amos)"),
+    "R-3R": new Missile("R-3R"),
+    "R-60M": new Missile("R-60M"),
+    "R-73 (AA-11 Archer)": new Missile("R-73 (AA-11 Archer)"),
     "R-77 (AA-12 Adder)": new Missile("R-77 (AA-12 Adder)"),
+    "SM-2": new Missile("SM-2"),
+
+//CBU-87
+//CBU-97
+//M-61A1
+//M56A3 20mm HEI
+
 };
