ad9e95366daaecef0f9fa3c441acfb66a6703c10
Dragonius <Blackerhawker@hotmail.com>
2025-02-15 01:21:39 +0200

    added || entity.types.includes("Weapon")

--- src/stores/TrackStore.tsx
+++ src/stores/TrackStore.tsx
@@ -92,7 +92,7 @@ export const trackStore = create<TrackStoreData>(() => {
 (window as any).trackStore = trackStore;
 
 function isEntityTrackable(entity: RawEntityData) {
-  return entity.types.includes("Air") && !entity.types.includes("Parachutist");
+  return entity.types.includes("Air") || entity.types.includes("Weapon") && !entity.types.includes("Parachutist");
 }
 
 export function createTracks(event: SneakerInitialStateEvent) {
